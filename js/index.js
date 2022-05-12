3e70121bdf5f4a6c43bba4f74382b52f79b77fcc
ratoobi <69602231+ratoobi@users.noreply.github.com>
2022-05-11 21:09:39 -0300

    Add page routing

--- js/index.js
+++ js/index.js
@@ -1,24 +1,40 @@
 const page = document.querySelector(".page");
 const body = document.querySelector("body");
-const navigationLinks = document.querySelectorAll("a");
 const routes =
 {
-  "/": { "route": "/pages/home.html", "image": "/images/mountains-universe01.png" },
-  "/universo": { "route": "/pages/universo.html", "image": "/images/mountains-universe02.png" },
-  "/exploracao": { "route": "/pages/exploracao.html", "image": "/images/mountains-universe03.png" },
+  "/": { "route": "/pages/home.html", "image": "/images/mountains-universe01.png", "name": ".home-page" },
+  "/universo": { "route": "/pages/universo.html", "image": "/images/mountains-universe02.png", "name": ".universo-page" },
+  "/exploracao": { "route": "/pages/exploracao.html", "image": "/images/mountains-universe03.png", "name": ".exploracao-page" },
 }
+let previousPage = "/";
+
 
 router = (route) => {
   fetch(route).then(response => response.text()).then(data => page.innerHTML = data)
 }
 
-navigationLinks.forEach(link => {
-  link.addEventListener("click", (e) => {
-    e.preventDefault();
-    link.classList.toggle("current-page");
-    window.history.pushState({}, "", link.href);
-    const { pathname } = window.location;
-    body.style.backgroundImage = `url(${routes[pathname].image})`;
-    router(routes[pathname].route);
-  })
+changeBackground = (pathname) => {
+  body.style.backgroundImage = `url(${routes[pathname].image})`;
+}
+
+handle = () => {
+  const { pathname } = window.location;
+  document.querySelector(`${routes[pathname].name}`).classList.toggle("current-page");
+  previousPage = pathname;
+  router(routes[pathname].route);
+  changeBackground(pathname);
+}
+
+changeRoute = (event) => {
+  event = event || window.event;
+  event.preventDefault();
+  event.target.href = event.target.href || event.target.parentElement.href;
+  document.querySelector(`${routes[previousPage].name}`).classList.toggle("current-page");
+  window.history.pushState({}, "", event.target.href);
+  handle();
+}
+
+window.addEventListener('popstate', () => {
+  document.querySelector(`${routes[previousPage].name}`).classList.toggle("current-page");
+  handle();
 });
